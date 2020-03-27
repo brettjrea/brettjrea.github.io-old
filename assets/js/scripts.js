3ddfea1ffb2f9793ef1b928faa3bc12d46a3bfd0
brettjrea <6890798+brettjrea@users.noreply.github.com>
2020-03-27 19:55:52 -0400

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -49,7 +49,7 @@ function toggleNav() {
 }
 
 // Touch sidenav
-var container = document.querySelector("#myContent");
+var container = document.querySelector("#hoverpad");
 
     container.addEventListener("touchstart", startTouch, false);
     container.addEventListener("touchmove", moveTouch, false);
